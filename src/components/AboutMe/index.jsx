9a3e9723746fa2b1105729b4e797008bbee39694
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-14 16:12:16 -0300

    add base of home page

---
+++ src/components/AboutMe/index.jsx
@@ -0,0 +1,51 @@
+import styles from "./AboutMe.module.css";
+
+function Box({ title, children }){
+    return (
+        <div className={ styles.box }>
+            <h1>{ title }</h1>
+            { children }
+        </div>
+    );
+}
+
+function Skills() {
+
+function GridElemt({ imgId, imgAlt }){
+    return (
+        <div className={ styles.skills}>
+            <img src={ imgId } alt={ imgAlt } />
+            <span>{ imgAlt }</span>
+        </div>
+    );
+}
+
+  return (
+    <div className={styles.skillGrid}>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"HTML 5"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"CSS 3"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"JavaScript"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"ReactJS"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"NodeJS"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"Figma"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"Chrome"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"VSCode"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"Git"}/>
+        <GridElemt imgId={"/images/HTML5.png"} imgAlt={"NPM"}/>
+    </div>
+  );
+}
+
+export default function AboutMeArea() {
+  return (
+    <div className={ styles.abmeArea }>
+        <Box title={"Sobre mim"}>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas repellat similique distinctio impedit. Suscipit voluptatem autem laboriosam architecto, iure itaque tempora recusandae aspernatur vitae blanditiis rerum! Dignissimos culpa incidunt aliquam.</p>
+        </Box>
+
+        <Box title={"Minhas habilidades"}>
+            <Skills />
+        </Box>
+    </div>
+  );
+}
