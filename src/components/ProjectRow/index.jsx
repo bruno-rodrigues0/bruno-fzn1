ce1493cbdce61605be67c2bd945d7e7c25f6751d
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-14 19:17:08 -0300

    make project cards work

--- src/components/ProjectRow/index.jsx
+++ src/components/ProjectRow/index.jsx
@@ -1,24 +1,21 @@
-import styles from './ProjectRow.module.css'
-import dbproj from '../../json/projetos.json'
+import styles from "./ProjectRow.module.css";
+import dbproj from "../../json/projetos.json";
 
-function ProjectCard(){
-    
-    return (
-        dbproj.map(projetos => 
-        <div className={ styles.projeto }>
-            <h3>{ projetos.title }</h3>
-            <span>{ projetos.tecnologies }</span>
-        </div>
-        )
-    );
+function ProjectCard({ titulo, tec }) {
+  return (
+    <div className={styles.projeto}>
+      <h3>{ titulo }</h3>
+      <span>{ tec }</span>
+    </div>
+  );
 }
 
-export default function ImageRow(){
-    return (
-        <div className={ styles.row }>   
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-        </div>
-    );
+export default function ImageRow() {
+  return (
+    <div className={styles.row}>
+      {dbproj.slice(0, 3).map((projetos) => (
+        <ProjectCard titulo={projetos.title} tec={projetos.tecnologies} />
+      ))}
+    </div>
+  );
 }
