7a8dcc3437733309f4198c1e110ba4ce8fed6f5b
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-19 17:22:52 -0300

    change app title

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,9 +2,11 @@ import styles from './Button.module.css'
 
 export default function Button({ text }){
     return(
-        <button className={ styles.button }>
-            <p> <span>-&gt; </span>{ text }</p>      
-        </button>
+        <a href="Projects.jsx">
+            <button className={ styles.button }>
+                <p> <span>-&gt; </span>{ text }</p>
+            </button>
+        </a>
     );
 
 }
