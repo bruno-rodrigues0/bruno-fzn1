3ea2924b7da05fbae694be683e5181c7e1ba3a15
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-19 13:23:44 -0300

    add image at project cards

--- src/components/ProjectRow/index.jsx
+++ src/components/ProjectRow/index.jsx
@@ -1,20 +1,31 @@
 import styles from "./ProjectRow.module.css";
 import dbproj from "../../json/projetos.json";
 
-function ProjectCard({ titulo, tec }) {
+export function ProjectCard({ titulo, tec, img }) {
   return (
-    <div className={styles.projeto}>
-      <h3>{ titulo }</h3>
-      <span>{ tec }</span>
-    </div>
+      <>    
+        <div 
+          className={styles.projeto} 
+          style={{ backgroundImage: `url("${ img }")` }}>
+          <h3>{ titulo }</h3>
+          <span>Tecnologias utilizadas: </span><br />
+          <ul>
+            <li><span>{ tec }</span></li>
+          </ul>
+          
+          </div>
+      </>
   );
 }
 
 export default function ImageRow() {
   return (
     <div className={styles.row}>
-      {dbproj.slice(0, 3).map((projetos) => (
-        <ProjectCard titulo={projetos.title} tec={projetos.tecnologies} />
+      {dbproj.map((projetos) => (
+        <ProjectCard 
+          titulo={projetos.title} 
+          tec={projetos.tecnologies} 
+          img={projetos.url} />
       ))}
     </div>
   );
