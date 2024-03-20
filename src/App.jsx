9ff445ea3fa2caa6ad011b2d91b4dacb697c79e8
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-20 12:07:04 -0300

    remove speedinsigths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom'
-import { SpeedInsigths } from '@vercel/speed-insights/react'
 
 export default function App() {
   return (
     <>
-      <SpeedInsigths />
       <Outlet />
     </>
   );
