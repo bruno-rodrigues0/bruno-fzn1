ea0e8f2f69fa7e6c615c1a8177d668824c9b3dd9
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-21 10:35:10 -0300

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   return (
     <>
       <SpeedInsights />
+      <Analytics />
       <Outlet />
       <Footer />
     </>
