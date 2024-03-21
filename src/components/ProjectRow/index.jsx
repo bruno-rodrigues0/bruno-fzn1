a1b44933839cb602caaf4b500c3715117f4321f2
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-21 13:01:47 -0300

    link projects to github respective repos

--- src/components/ProjectRow/index.jsx
+++ src/components/ProjectRow/index.jsx
@@ -1,11 +1,12 @@
 import styles from "./ProjectRow.module.css";
 import dbproj from "../../json/projetos.json";
+import { Link } from 'react-router-dom'
 
-export function ProjectCard({ titulo, tec, img }) {
+export function ProjectCard({ titulo, tec, img, link }) {
   tec = [...tec]
   return (   
-        <div 
-          className={styles.projeto} 
+        <Link to={ link } target="_blank"
+          className={ styles.projeto } 
           style={{ backgroundImage: `url("${ img }")` }}>
 
           <h3>{ titulo }</h3>
@@ -17,18 +18,19 @@ export function ProjectCard({ titulo, tec, img }) {
             }) }
           </ul>
           
-        </div>
+        </Link>
   );
 }
 
 export default function ImageRow() {
   return (
-    <div className={styles.row}>
+    <div className={ styles.row }>
       {dbproj.slice(0, 3).map((projetos) => (
         <ProjectCard 
-          titulo={projetos.title} 
-          tec={projetos.tecnologies} 
-          img={projetos.url} />
+          titulo={ projetos.title } 
+          tec={ projetos.tecnologies } 
+          img={ projetos.url } 
+          link={ projetos.link }/>
       ))}
     </div>
   );
