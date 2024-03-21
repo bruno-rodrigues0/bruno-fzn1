764246d32121a7dcbadd7254a4fd8f082e8b2df8
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-21 10:29:07 -0300

    add speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
-import { SpeedInsights } from '@vercel/speed-insights/*';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
   return (
